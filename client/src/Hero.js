7ad7ac84fd71bc546a734daec516edf99d5b2cd1
pat-white-code <pat.k.white@gmail.com>
2020-08-18 10:51:53 -0500

    adds header and subheader

--- client/src/Hero.js
+++ client/src/Hero.js
@@ -4,7 +4,10 @@ import styles from './Hero.module.css';
 const Hero = () => {
   return (
     <div className={styles['hero-container']}>
-      <div className={styles['col-1']}></div>
+      <div className={styles['col-1']}>
+        <h1 className={styles['hero-h1']}>Terry's Taco Joint</h1>
+        <h3 className={styles['hero-h4']}>Pretty Good Tacos!</h3>
+      </div>
 
       <div className={styles['col-2']}>
         <div className={styles['item-1']}></div>
